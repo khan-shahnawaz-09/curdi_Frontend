19a6181e79c03aa3a2706f341d9f06dbe300b3a2
khan-shahnawaz-09 <khan.dev006@gmail.com>
2025-08-19 13:08:06 +0530

    add the comment to my navbar

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900">
-      {/* Navbar */}
+      {/* Navbar... */}
       <Nav setPopup={setPopup} />
 
       <div className="flex flex-1">
